647ccf0918f767e0db4baa40f3bb133ea9acdfbc
Enrico Rufnak <enrruf@web.de>
2016-04-24 18:09:41 +0200

    Changed shell script to wait until key enter pressed to proceed script

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ cd setup
 git clone https://github.com/enricorufnak/provision-local.git
 cd provision-local
 
+echo -e "Edit tasks files to change installation process and press [ENTER] to proceed ..." && read USELESS_VAR
+
 ansible-playbook -i hosts setup.yml --ask-sudo-pass
 
 source ~/.bashrc
