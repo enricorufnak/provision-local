7bdf38b2a32102a9d1d45ea2436aeca0d53c2257
Enrico Rufnak <enrruf@web.de>
2022-04-02 18:54:54 +0200

    Added script for Windows powershell to disable services and updated bootstrap

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -4,3 +4,4 @@ ls -Recurse *.ps*1 | Unblock-File
 
 #. $PSScriptRoot\scripts\remove-default-apps.ps1
 #. $PSScriptRoot\scripts\identity.ps1
+#. $PSScriptRoot\scripts\disable-services.ps1
---
+++ windows/scripts/disable-services.ps1
@@ -0,0 +1,51 @@
+#   Description:
+# This script disables unwanted Windows services. If you do not want to disable
+# certain services comment out the corresponding lines below.
+
+Import-Module -DisableNameChecking $PSScriptRoot\..\modules\Verify-Elevated.psm1
+
+if (!(Verify-Elevated)) {
+    # We are not running as an administrator, so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
+    $newProcess.Arguments = "-noexit & '" + $script:MyInvocation.MyCommand.Path + "'"
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    Exit;
+}
+
+$services = @(
+    "diagnosticshub.standardcollector.service" # Microsoft (R) Diagnostics Hub Standard Collector Service
+    "DiagTrack"                                # Diagnostics Tracking Service
+    "dmwappushservice"                         # WAP Push Message Routing Service (see known issues)
+    "lfsvc"                                    # Geolocation Service
+    "MapsBroker"                               # Downloaded Maps Manager
+    "NetTcpPortSharing"                        # Net.Tcp Port Sharing Service
+    "RemoteAccess"                             # Routing and Remote Access
+    "RemoteRegistry"                           # Remote Registry
+    "SharedAccess"                             # Internet Connection Sharing (ICS)
+    "TrkWks"                                   # Distributed Link Tracking Client
+    "WbioSrvc"                                 # Windows Biometric Service (required for Fingerprint reader / facial detection)
+    #"WlanSvc"                                 # WLAN AutoConfig (Disabling this can cause issues with wifi connectivity)
+    #"WMPNetworkSvc"                           # Windows Media Player Network Sharing Service (Unknown service windows 11)
+    #"wscsvc"                                  # Windows Security Center Service
+    #"WSearch"                                 # Windows Search
+    "XblAuthManager"                           # Xbox Live Auth Manager
+    "XblGameSave"                              # Xbox Live Game Save Service
+    "XboxNetApiSvc"                            # Xbox Live Networking Service
+    "ndu"                                      # Windows Network Data Usage Monitor
+    # Services which cannot be disabled
+    #"WdNisSvc"
+)
+
+foreach ($service in $services) {
+    Write-Output "Trying to disable $service"
+    Get-Service -Name $service | Set-Service -StartupType Disabled
+}
